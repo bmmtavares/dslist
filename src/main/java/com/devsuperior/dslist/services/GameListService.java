82f772bed909b3d5e603e6619432cab8a14771f1
bmmtavares <brunommtavares@gmail.com>
2023-05-10 08:17:16 -0300

    GET lists

---
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.repositories.GameListRepository;
+
+// Regra de Negocios
+
+@Service
+public class GameListService {
+
+	// injetando, dependencia com bd
+	@Autowired
+	private GameListRepository gameListRepository;
+
+	
+	@Transactional(readOnly = true)
+	public List<GameListDTO> findAll() {
+		// var result = gameRepository.findAll();
+		List<GameList> result = gameListRepository.findAll();
+		List<GameListDTO> dto = result.stream().map(x -> new GameListDTO(x)).toList();
+		return dto;
+	}
+}
